146e97b7a780dccea37e4e3b1d98cf030cdc8e4c
luca-stamatescu <developer@lucastamatescu.com>
2019-12-12 19:28:56 +1100

    make single pickle file for processed data and use python docker image

--- src/data/data_optimizer_script.py
+++ src/data/data_optimizer_script.py
@@ -44,18 +44,18 @@ def optimize_chunk(chunk,i,original_data_size,new_data_size):
     print("The initial space on disk was (MB): " ,round(initial_memory_usage ,2) ," and the final space on disk is (MB): "
           ,round(final_memory_usage ,2))
 
-    # ### Store data as a pickle
-    # By storing the data as a pickle, the size on disk is reduced to 67% of the original size, and loads 10x faster.
-    sample_data.to_pickle("../../data/processed/memory_optimized_data"+str(i)+".pkl")
+
 
     original_data_size+=initial_memory_usage
     new_data_size+=final_memory_usage
 
-    return original_data_size,new_data_size
+    return original_data_size,new_data_size,sample_data
 
 
 
 i = 0
+
+final_data=None
 original_data_size=0
 new_data_size=0
 chunksize = 10 ** 6
@@ -63,7 +63,13 @@ for chunk in pd.read_csv("../../data/raw/train.csv", chunksize=chunksize):
     i += 1
     print(i)
     print(chunk.head())
-    original_data_size, new_data_size=optimize_chunk(chunk,i,original_data_size,new_data_size)
+    original_data_size, new_data_size,sample_data=optimize_chunk(chunk,i,original_data_size,new_data_size)
     print("The total original data size was: "+str(round(original_data_size,2))+" and the new total data size is: "+str(round(new_data_size,2)))
+    if  i==1:
+        final_data=sample_data
+    final_data.append(sample_data)
     # if i == 2:
     #     break
+# ### Store data as a pickle
+    # By storing the data as a pickle, the size on disk is reduced to 67% of the original size, and loads 10x faster.
+final_data.to_pickle("../../data/processed/memory_optimized_data.pkl")
